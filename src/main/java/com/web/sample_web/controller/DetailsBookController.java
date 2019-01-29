ae099fcb52149ded5427c4acd4d200a632d9924d
alireza <alirezatl135@gmail.com>
2019-01-29 12:56:05 -0800

    added modal for login with bootstrap4

--- src/main/java/com/web/sample_web/controller/DetailsBookController.java
+++ src/main/java/com/web/sample_web/controller/DetailsBookController.java
@@ -7,9 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
 import java.util.Base64;
+import java.util.List;
 
 @Controller
 public class DetailsBookController {
@@ -18,10 +21,12 @@ public class DetailsBookController {
     BookDao bookDao;
 
     @GetMapping(path = "/books/{bookCode}")
-    public String showBookDetails(@PathVariable String bookCode, ModelMap modelMap) {
+    public String showBookDetails(@PathVariable String bookCode, ModelMap modelMap, HttpSession session) {
+        List<Book> orderBook = (List<Book>)session.getAttribute("orderBook");
         Book bookDetail = bookDao.getByBookCode(bookCode);
         bookDetail.setPicBase64(Base64.getEncoder().encodeToString(bookDetail.getBookPicture()));
         modelMap.addAttribute("bookDetail",bookDetail);
+        modelMap.addAttribute("order",orderBook);
         return "details";
     }
 }
